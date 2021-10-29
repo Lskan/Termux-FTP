5d0a8d93409c5f0e616c45e6caa2d2a8e2acb4c1
Lskan <93380878+Lskan@users.noreply.github.com>
2021-10-29 14:19:20 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+
+#colors
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+#started ftp
+string="Установка FTP...."
+echo $GREEN $string
+echo $WHITE
+pkg install busybox
+string1="Запуск успешен!"
+string2="Инструкцию можете найти в файле conf.txt"
+echo $RED $string2
+echo $GREEN $string1
+echo $WHITE
+echo "Для того, что бы зайти в FTP скачайте любой FTP проводник на свое устройство, далее захожим в него в и поле IP надо вести '0.0.0.0' в поле порт надо вести '1024' и в поле логин/имя пользователя водим 'root' "> conf.txt
+busybox tcpsvd -vE 0.0.0.0 1024 busybox ftpd -w ~
+
